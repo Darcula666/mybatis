61ceacacbeac67e3524e7a988b09ffed8fc52e52
小布123 <1172504039@qq.com>
2018-06-04 21:16:43 +0800

    增删改查

--- src/test/java/com/gfdz/mybatis/MybatisTest.java
+++ src/test/java/com/gfdz/mybatis/MybatisTest.java
@@ -1,6 +1,9 @@
 package com.gfdz.mybatis;
 
+import com.gfdz.mybatis.dao.EmployeeMapper;
+import com.gfdz.mybatis.entity.Employee;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
@@ -19,6 +22,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MybatisTest {
+    /**
+     * 1.根据xml配置文件（全局配置文件）创建一个SqlSessionFactory
+     * @return
+     * @throws IOException
+     */
     public SqlSessionFactory getSqlSessionFactory() throws IOException {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
@@ -34,5 +42,27 @@ public class MybatisTest {
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
         MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
         myBatisGenerator.generate(null);
+    }
+    @Test
+    public  void testAdd() throws IOException {
+        //2.获取sqlSession实例，能直接执行已经映射的sql语句
+
+        SqlSessionFactory sessionFactory=  getSqlSessionFactory();
+        SqlSession sqlSession=getSqlSessionFactory().openSession();
+        try {
+            //sql唯一标识：statement
+            //执行sql要用的参数
+            EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+            Employee employee = mapper.getById(1);
+            System.out.println(employee);
+        }finally {
+            sqlSession.close();
+        }
+
+    }
+    @Test
+    public  void testSelect() throws IOException {
+      SqlSessionFactory sessionFactory=  getSqlSessionFactory();
+        SqlSession sqlSession=getSqlSessionFactory().openSession();
     }
 }
